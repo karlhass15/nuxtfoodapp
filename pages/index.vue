4c3ba8676e1fbc2d62d83c89b0481050e1100401
Karl Hass <karl.hass15@gmail.com>
2021-07-14 18:51:58 -0500

    inital commit

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <main class="conatiner">
+    <AppHeader />
+    <AppRestaurantInfo />
+  </main>
+</template>
+
+<script>
+import AppHeader from '@/components/AppHeader.vue';
+import AppRestaurantInfo from '@/components/AppRestaurantInfo.vue';
+
+export default {
+  components: {
+    AppRestaurantInfo,
+    AppHeader
+  },
+};
+
+</script>
